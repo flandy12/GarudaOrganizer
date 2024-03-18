e097c51bdfaa5335a0960ecdbb50a0f5d4127f3b
flandy12 <halloflandy@gmail.com>
2024-03-18 11:14:10 +0700

    Validation Maintenance Page & Responsive Page

--- src/page/error/maintenance.js
+++ src/page/error/maintenance.js
@@ -1,12 +1,12 @@
 const MaintenancePage = () => {
     return (
         <>
-            <div className="flex flex-col justify-center py-20">
+            <div className="flex flex-col justify-center py-20  max-md:px-5">
                 <picture>
                     <source media="(min-width: 650px)" srcSet={'/images/asset/maintenance.png'} />
                     <img src={'/images/asset/maintenance.png'} alt="maintenance-asset" className="m-auto"/>
                 </picture>
-                <div className="max-w[400px] w-[610px] text-center mx-auto mt-5 space-y-3">
+                <div className="md:max-w-[auto] xl:w-[610px] lg:w-[610px] text-center mx-auto mt-5 space-y-3 xl:px-0 lg:px-0 max-md:px-5 md:px-20">
                     <h1 className="font-bold text-2xl text-blue-500">Site Under Maintenance</h1>
                     <p>Apologies for the inconvenience. Our website is currently undergoing maintenance to enhance user experience. We will be back online shortly. Thank you for your patience and understanding.</p>
                 </div>
